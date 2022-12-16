a6f3dc1a473af99cb87e5413d72ff285fd573119
Micaela Perez <micaelaperez663@gmail.com>
2022-12-16 19:28:17 -0300

    adding a filter by status

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
 
-export default function IssuesList({ labels }) {
-  const issuesQuery = useQuery(["issues", { labels }],
+export default function IssuesList({ labels, status }) {
+  const issuesQuery = useQuery(["issues", { labels, status }],
     () => {
+      const statusString = status ? `&status=${status}` : "";
+
       const labelsString = labels.map((label) =>
         `labels[]=${label}`).join("&");
 
-      return fetch(`https://ui.dev/api/courses/react-query/issues?${labelsString}`)
+      return fetch(`https://ui.dev/api/courses/react-query/issues?${labelsString}${statusString}`)
         .then(res => res.json())
     }
   );
